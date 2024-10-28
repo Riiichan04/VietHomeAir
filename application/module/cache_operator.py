712c45641b78953e565f626612904ebf26c0f181
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-10-28 13:42:26 +0700

    Complete fetch third-party API by each hour

--- application/module/cache_operator.py
+++ application/module/cache_operator.py
@@ -1,4 +1,4 @@
 import os, json, time
 
-def is_need_renew_weather_cache_data(json):
-    return True if not 'weather' in json else int(time.time()) - json['weather_data']["upload_time"] >= 3600
+def is_need_renew_weather_cache_data(json_file):
+    return True if not 'weather_data' in json_file else int(time.time()) - json_file['weather_data']["upload_time"] >= 3600
