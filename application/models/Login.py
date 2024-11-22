b20b90df2655f740188f2ffc3e31a8a538eda828
NguyenHuuThinh <22130269@st.hcmuaf.edu.vn>
2024-11-22 17:13:58 +0700

    login page

---
+++ application/models/Login.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+
+
+
+
