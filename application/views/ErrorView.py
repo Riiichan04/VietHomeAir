ac1c8d57580908df8eb52cb1ef86631ea579817f
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-24 17:52:58 +0700

    Reuse 404 error page to all other error pages

--- application/views/ErrorView.py
+++ application/views/ErrorView.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 
+context = {
+    'status_code': 404  # Mặc định hiển thị lỗi 404
+}
+
 
 def error_400(request, exception=None):
-    return render(request, 'application/templates/error-404.html', exception, {}, status=400)
+    context["status_code"] = 400
+    return render(request, 'application/templates/error.html', exception, context, status=400)
 
 
 def error_403(request, exception=None):
-    return render(request, 'application/templates/error-404.html', exception, {}, status=403)
+    context["status_code"] = 403
+    return render(request, 'application/templates/error.html', exception, context, status=403)
 
 
 def error_404(request, exception):
-    return render(request, 'application/templates/error-404.html', {}, status=404)
+    context["status_code"] = 404
+    return render(request, 'application/templates/error.html', context, status=404)
 
 
 def error_500(request, exception=None):
-    return render(request, 'application/templates/error-404.html', exception, {}, status=500)
+    context["status_code"] = 500
+    return render(request, 'application/templates/error.html', exception, context, status=500)
