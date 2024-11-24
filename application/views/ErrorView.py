f0fcf3c3837e8b84e59e45255f09d40b15ecea59
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-25 02:25:26 +0700

    Change image and add error404 and error500 page

--- application/views/ErrorView.py
+++ application/views/ErrorView.py
@@ -7,17 +7,17 @@ context = {
 
 
 def get_error_400_page(request, exception=None):
-    return render(request, 'application/templates/error.html', context, status=400)
+    return render(request, 'application/templates/error404.html', context, status=400)
 
 
 def get_error_403_page(request, exception=None):
-    return render(request, 'application/templates/error.html', context, status=403)
+    return render(request, 'application/templates/error404.html', context, status=403)
 
 
 def get_error_404_page(request, exception=None):
-    return render(request, 'application/templates/error.html', context, status=404)
+    return render(request, 'application/templates/error404.html', context, status=404)
 
 
 def get_error_500_page(request, exception=None):
     context["status_code"] = 500
-    return render(request, 'application/templates/error.html', context, status=500)
+    return render(request, 'application/templates/error404.html', context, status=500)
