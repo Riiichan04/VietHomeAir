d13924b1454a0fe9227aac7f4f4a448b05073b9a
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-24 17:53:52 +0700

    Reuse 404 error page to all other error pages

--- application/views/ErrorView.py
+++ application/views/ErrorView.py
@@ -9,16 +9,13 @@ def error_400(request, exception=None):
     context["status_code"] = 400
     return render(request, 'application/templates/error.html', exception, context, status=400)
 
-
 def error_403(request, exception=None):
     context["status_code"] = 403
     return render(request, 'application/templates/error.html', exception, context, status=403)
 
-
 def error_404(request, exception):
     context["status_code"] = 404
     return render(request, 'application/templates/error.html', context, status=404)
-
 
 def error_500(request, exception=None):
     context["status_code"] = 500
