4b21302332ea7beda5cb156be54d8dba3df2ce18
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-02 00:42:48 +0700

    Config public directory

--- bed_and_breakfast/settings.py
+++ bed_and_breakfast/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'bed_and_breakfast.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, "template", os.path.join(BASE_DIR, 'global_template')],  #Quy định các file global
+        'DIRS': [BASE_DIR, "template", os.path.join(BASE_DIR, 'public')],  #Quy định các file global
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = 'public/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ public/static/scripts/product.js
@@ -0,0 +1,7 @@
+$("#close-popup-btn").click(function(){
+    $(".popup-container").css("display", "none")
+})
+
+$("#show-description-popup").click(function (){
+    $(".popup-container").css("display", "block")
+})
