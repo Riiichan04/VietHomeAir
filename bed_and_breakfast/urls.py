4ecf143ee3487c07878e355233d91689f45c439d
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-12-03 10:56:22 +0700

    Giải quyết conflict

--- bed_and_breakfast/urls.py
+++ bed_and_breakfast/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from application.views import AboutUsView
 from application.views.PolicyView import PolicyViews
 from application.views.ProductView import ProductView
 from application.views.AboutUsView import AboutUsView
 from application.views.ContactView import ContactView
+from application.views.LoginView import LoginView
 from application.views.HomeView import HomeView
 
 # Lưu ý: Nếu muốn hiển thị các trang lỗi custom thì phải set DEBUG = False và phải set ALLOWED_HOSTS
@@ -41,6 +41,7 @@ urlpatterns = [
     path('privacy/', PolicyViews.as_view(template_name='policy/privacy-policy.html'), name='privacy-policy'),
     path('terms-of-use/', PolicyViews.as_view(template_name='policy/terms.html'), name='terms-of-use'),
     path('policy/', PolicyViews.as_view(template_name='policy/other-policy.html'), name='terms-of-use'),
+    path('login/', LoginView.as_view(), name='login'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
