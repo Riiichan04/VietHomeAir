32089823689f09c1be6b1098b83a4cc0396c0b47
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-09 18:36:11 +0700

    Edit csv file operation

--- application/module/process_json_data.py
+++ application/module/process_json_data.py
@@ -5,13 +5,13 @@ with open('../static/data.json', 'r', encoding='utf-8') as file:
     list_data = json.load(file)
 
 for data in list_data:
-    data.pop('description')
+    # data.pop('description')
     for index in range(len(data['policy'])):
         data['policy'][index] = data['policy'][index].replace('Nội quy nhà\n', '').replace('\nHiển thị thêm', '')
 
 
 with open('../static/convert_data.csv', 'w', encoding='utf-8', newline='') as csv_file:
-    fieldnames = ['title', 'imageUrl', 'listService', 'price', 'comment', 'policy']
+    fieldnames = ['title', 'description' , 'imageUrl', 'listService', 'price', 'comment', 'policy']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(list_data)
