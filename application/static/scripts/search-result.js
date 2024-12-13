ffd8c33e6f8ebadf63c2d98aa631d75b1733c08d
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2024-12-13 23:10:05 +0700

    Xong trang tìm kiếm.

--- application/static/scripts/search-result.js
+++ application/static/scripts/search-result.js
@@ -5,3 +5,7 @@ $("#filter").click(function () {
 $(".btn-close").click(function (){
     $('#filter-container').addClass('d-none');
 })
+$(".select-option").click(function (){
+    console.log("hehehehehehehhe")
+    $(this).addClass("active");
+})
