41d950be45d12ffff1a8fe96aa8e54fbfbea32f2
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-15 05:11:25 +0700

    Tạm hoàn thiện phần View (Controller) của trang

--- application/views/BnbInfoView.py
+++ application/views/BnbInfoView.py
@@ -12,8 +12,8 @@ class BnbInfoView(TemplateView):
     template_name = "application/templates/bnb-information.html"
 
     def get_context_data(self, **kwargs):
-        bnb = bnb_processing.get_bnb(self.kwargs['bnbid'])
-        if bnb is None: raise Http404()
+        bnb = bnb_processing.get_full_bnb_info(self.kwargs['bnbid'])
+        if bnb['bnb_info'] is None: raise Http404("Eooooo, tìm nhầm chỗ rồi")
         context = super().get_context_data(**kwargs)
         context['bnb'] = bnb
         return context
