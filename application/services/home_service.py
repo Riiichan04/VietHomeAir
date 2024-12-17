dd7b9d40f644347507ed4a162bf1dae686744d79
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-18 00:21:19 +0700

    Hoàn thiện phần query cho trang chủ

--- application/services/home_service.py
+++ application/services/home_service.py
@@ -7,17 +7,18 @@ def get_bnb_display_element(bnb_id):
     bnb = BnbInformation.objects.filter(status=True).filter(id=bnb_id).first()
     if bnb is None: return None
     return {
+        'id': bnb.id,
         'name': bnb.name,
         'description': bnb.description,  # Xử lý sau
         'thumbnail': bnb.image_set.first().url,
-        'owner': bnb.owner.account,
+        'owner': bnb.owner.account.fullname,
         'avg_rating': str(calculate_avg_bnb_rating([review for review in bnb.review_set.all()]))
     }
 
 
 # Tính toán rating trung bình của bnb
 def calculate_avg_bnb_rating(bnb_reviews):
-    return sum(map(lambda x: x.rating, bnb_reviews)) / len(bnb_reviews)
+    return round(sum(map(lambda x: x.rating, bnb_reviews)) / len(bnb_reviews), 2)
 
 
 # Lấy id của bnb có lượt xem cao nhất
@@ -38,5 +39,6 @@ def get_most_rated_bnb():
         where info.status = True        
         limit 5
     """
+
     list_id = [bnb.id for bnb in BnbInformation.objects.raw(query)]
-    return list(BnbInformation.objects.filter(id__in=list_id).values_list('id'))
+    return [bnb.id for bnb in BnbInformation.objects.filter(id__in=list_id).all()]
