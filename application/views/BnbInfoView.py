f587625db46fc232c1d3ac6646d554cc9a40bc5d
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-17 17:26:17 +0700

    Chuyển phần xử lý business logic sang services

--- application/views/BnbInfoView.py
+++ application/views/BnbInfoView.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.views.generic import TemplateView
 from application.models import BnbInformation
-import application.module.bnb_processing as bnb_processing
+import application.services.bnb_info_service as bnb_service
 
 
 class SampleBnbInfoView(TemplateView):
@@ -12,7 +12,7 @@ class BnbInfoView(TemplateView):
     template_name = "application/templates/bnb-information.html"
 
     def get_context_data(self, **kwargs):
-        bnb = bnb_processing.get_full_bnb_info(self.kwargs['bnbid'])
+        bnb = bnb_service.get_full_bnb_info(self.kwargs['bnbid'])
         if bnb['bnb_info'] is None: raise Http404("Eooooo, tìm nhầm chỗ rồi")
         context = super().get_context_data(**kwargs)
         # Code tạm để mai báo cáo
