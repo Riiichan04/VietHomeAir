de265992db3d3990548c7f1b02afa156798bc62e
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2024-12-17 11:30:09 +0700

    Xong FE trang User (tt)

--- application/views/UserView.py
+++ application/views/UserView.py
@@ -1,7 +1,24 @@
 from django.views.generic import TemplateView
 
+
 class UserView(TemplateView):
-    template_name = "application/templates/user.html" #default view
+    template_name = "application/templates/user.html"  #default view
+
 
 class UserInfoView(TemplateView):
     template_name = 'application/templates/user/user-information.html'
+
+
+class UserOrderHistoryView(TemplateView):
+    template_name = 'application/templates/user/user-order-history.html'
+
+
+class UserViewedHistoryView(TemplateView):
+    template_name = 'application/templates/user/user-viewed-history.html'
+
+
+class UserReviewHistoryView(TemplateView):
+    template_name = 'application/templates/user/user-reviewed-history.html'
+
+class UserWishListView(TemplateView):
+    template_name = 'application/templates/user/user-wishlist.html'
