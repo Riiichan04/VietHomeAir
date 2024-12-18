c2d0a4231da564b2d4b0e99f2e0587673b337fed
NguyenHuuThinh <22130269@st.hcmuaf.edu.vn>
2024-12-18 16:01:07 +0700

    Up file Owner-Register

--- application/views/OwnerRegister.py
+++ application/views/OwnerRegister.py
@@ -1,4 +1,9 @@
 from django.views.generic import  TemplateView
+from django.shortcuts import render
 
 class OwnerRegisterView(TemplateView):
     template_name = 'application/templates/OwnerRegister.html'
+
+
+def home(request):
+    return render(request, 'home.html')
