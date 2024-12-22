03b4e6bb035395c2cc4a71a35e5022281ce43e42
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-22 14:55:07 +0700

    Thêm phần wishlist bnb vào session

--- application/templatetags/bnb_info_filter.py
+++ application/templatetags/bnb_info_filter.py
@@ -12,6 +12,15 @@ def current_user(user, session):
     return session.user if session.get('user') is not None else None
 
 
+@register.simple_tag(name='save_bnb_to_wishlists')
+def save_bnb_to_wishlists(session, bnb_id):
+    if session.get('user') is None or session.get('wishlists') is None: return None
+    wishlist_session = session.get('wishlists')
+    if bnb_id in wishlist_session: return wishlist_session
+    wishlist_session.append(bnb_id)
+    return wishlist_session
+
+
 @register.simple_tag
 def user_review_status(session, bnb):
     if session.get('user') is None or bnb is None: return False
