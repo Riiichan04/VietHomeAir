1b8dfd96292266553c000291515c6669e2c61d64
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2024-12-23 23:53:58 +0700

    Xử lý service, view để hiển thị kết quả.

---
+++ application/services/search_service.py
@@ -0,0 +1,18 @@
+from application.models import BnbInformation
+from application.services.home_service import get_bnb_display_element
+
+
+# Hiển thị thông tin bnb
+def get_bnb_result_info(bnb_id):
+    return get_bnb_display_element(bnb_id)
+# Lấy bnb theo kết quả tìm kiếm
+def get_bnb_result(searchkey):
+    query="""
+       SELECT id
+    FROM application_bnbinformation as bnb
+    WHERE bnb.status = TRUE
+    AND (bnb.name LIKE %s)
+    LIMIT 5
+    """
+    list_id = [bnb.id for bnb in BnbInformation.objects.raw(query, [f"%{searchkey}%"])]
+    return [bnb.id for bnb in BnbInformation.objects.filter(id__in=list_id).all()]
--- application/views/ResultView.py
+++ application/views/ResultView.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
-
+from django.http import Http404, JsonResponse
+from application.services.search_service import get_bnb_result_info,get_bnb_result
 class ResultView(TemplateView):
     template_name = "application/templates/search-result.html"
+
+    def get_context_data(self, searchkey, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Tìm theo tên nhà
+        context['searchkey'] =[get_bnb_result_info(bnb_id) for bnb_id in get_bnb_result(searchkey)]
+        return context
