c519125e92ab3d4de3aa8d9cc455cf9157d7398c
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-24 13:35:27 +0700

    Chuẩn bị data để báo cáo

--- bed_and_breakfast/settings.py
+++ bed_and_breakfast/settings.py
@@ -132,7 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 SESSION_COOKIE_HTTPONLY = True
+
 # Allow url nào được cho phép dùng CORS
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3110",
