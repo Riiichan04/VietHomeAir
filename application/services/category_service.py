f321113552d39514d3c992948fee74171e00346f
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-01-06 19:50:20 +0700

    Thêm phần hiển thị product và bản đồ

--- application/services/category_service.py
+++ application/services/category_service.py
@@ -8,16 +8,19 @@ vietnamese_characters = str.maketrans(
     "A" * 17 + "D" + "E" * 11 + "I" * 5 + "O" * 17 + "U" * 11 + "Y" * 5 + "a" * 17 + "d" + "e" * 11 + "i" * 5 + "o" * 17 + "u" * 11 + "y" * 5
 )
 
+def get_category(category_name: str):
+    category_filter_result = [category for category in Category.objects.all() if
+                              remove_vietnamese_characters(category.name).replace(' ', '').lower() ==
+                              remove_vietnamese_characters(category_name).replace(' ', '').lower()]
+    return category_filter_result[0] if category_filter_result != [] else None
 
-def get_category(category_name):
-    return [category for category in Category.objects.all() if
-            remove_vietnamese_characters(category.name) == category_name]
-
+def get_category_original_name(category):
+    return category.name
 
 def get_bnb_by_category(category_name):
     category = get_category(category_name)
     if category is None: return None
-    return [get_bnb_display_element(bnb.id) for bnb in BnbInformation.objects.filter(category=category[0])]
+    return [get_bnb_display_element(bnb.id) for bnb in BnbInformation.objects.filter(category=category)]
 
 
 def remove_vietnamese_characters(txt: str):
--- application/views/CategoryView.py
+++ application/views/CategoryView.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.views.generic import TemplateView
 
-from application.services.category_service import get_category, get_bnb_by_category
+from application.services.category_service import get_category, get_bnb_by_category, get_category_original_name
 
 
 class CategoryView(TemplateView):
@@ -9,9 +9,10 @@ class CategoryView(TemplateView):
 
     def get_context_data(self, **kwargs):
         category_name = self.kwargs['category_name']
-        if get_category(category_name) is None: raise Http404("Tìm cái j thế?")
+        category = get_category(category_name)
+        if category is None: raise Http404("Tìm cái j thế?")
         list_bnb_data = get_bnb_by_category(category_name)
         context = super().get_context_data(**kwargs)
-        context['category_name'] = category_name
+        context['category_name'] = get_category_original_name(category)
         context['list_bnb_data'] = list_bnb_data
         return context
