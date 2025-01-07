e497844519d7e22a55bc6c8acdc998211f05b4fa
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2025-01-07 16:54:44 +0700

    Trả sai kết quả danh sách bnb có tên chứa chuỗi

--- application/services/search_service.py
+++ application/services/search_service.py
@@ -1,19 +1,23 @@
-from application.models import BnbInformation
 from application.services.home_service import get_bnb_display_element
+from application.models.bnb import BnbInformation
+import unicodedata
 
 
-# Hiển thị thông tin bnb
-def get_bnb_result_info(bnb_id):
-    return get_bnb_display_element(bnb_id)
-# Lấy bnb theo kết quả tìm kiếm
-def get_bnb_result(searchkey):
-    query="""
-        SELECT bnb.id
-        FROM application_bnbinformation as bnb
-        WHERE bnb.status = TRUE
-        AND (bnb.name LIKE ?)
-        LIMIT 5
-    """
-    list_id= [bnb.id for bnb in BnbInformation.objects.raw(query, ['%' + searchkey + '%', True])]
-    print(list_id)
-    return [bnb.id for bnb in BnbInformation.objects.filter(id__in=list_id).all()]
+vietnamese_characters= str.maketrans("ÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬĐÈÉẺẼẸÊẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴáàảãạăắằẳẵặâấầẩẫậđèéẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ",
+    "A" * 17 + "D" + "E" * 11 + "I" * 5 + "O" * 17 + "U" * 11 + "Y" * 5 + "a" * 17 + "d" + "e" * 11 + "i" * 5 + "o" * 17 + "u" * 11 + "y" * 5)
+
+def get_search(query:str):
+    search_results= [bnb for bnb in BnbInformation.objects.all()
+                     if remove_vietnamese_characters(bnb.name).replace(' ','').lower().__contains__(remove_vietnamese_characters(query).replace(' ','').lower())]
+    return search_results[0] if search_results!=[] else None
+
+
+def get_bnb_by_result(query):
+    searchKey= get_search(query)
+    if searchKey is None: return None
+    return [get_bnb_display_element(bnb.id) for bnb in BnbInformation.objects.filter(name__icontains=searchKey)] #ham rong
+
+def remove_vietnamese_characters(query:str):
+    if not unicodedata.is_normalized("NFC",query):
+        query= unicodedata.normalize("NFC",query)
+    return query.translate(vietnamese_characters)
--- application/views/ResultView.py
+++ application/views/ResultView.py
@@ -1,21 +1,16 @@
 from django.views.generic import TemplateView
 from django.http import Http404, JsonResponse
-from application.services.home_service import get_bnb_display_element
-from application.models import BnbInformation
+from application.services.search_service import get_search, get_bnb_by_result
 class ResultView(TemplateView):
     template_name = "application/templates/search-result.html"
 
     def get_context_data(self, **kwargs):
+        query = self.kwargs['query']
+        results= get_search(query)
+        if not results:
+            raise Http404("Hả")
+        list_bnb = get_bnb_by_result(query)
         context = super().get_context_data(**kwargs)
-        query = self.kwargs.get('query')
-        # results = get_bnb_result(query)
-        # context['results'] = results
-        results= BnbInformation.objects.filter(name__contains=query).all()
-        list_bnb = [get_bnb_display_element(result.id) for result in results]
         context['list_bnb'] = list_bnb
         return context
-        # Tìm theo tên nhà
-        # context['query'] =[get_bnb_result_info(bnb_id) for bnb_id in get_bnb_result(query)]
-
-    # def get(self, request, *args, **kwargs):
 
