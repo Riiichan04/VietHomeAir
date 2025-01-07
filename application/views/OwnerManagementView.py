22d00b87d83475d217c55b2980444f4b8573a6bc
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2025-01-07 18:26:00 +0700

    Hoàn thành trang quản lý bnb cuả chủ nhà

--- application/views/OwnerManagementView.py
+++ application/views/OwnerManagementView.py
@@ -1,8 +1,9 @@
+from cloudinary.uploader import upload
 from django.http.response import Http404, JsonResponse
 from django.views.generic import TemplateView
 
 from application.models import bnb, BnbInformation
-from application.models.bnb import Image
+from application.models.bnb import Image, Category, Service, Rule
 from application.services.owner_management_service import (get_info_owner, get_list_info_bnb,
                                                            get_bnb_by_id, get_list_category,
                                                            get_list_service, get_list_rule, get_bnb)
@@ -59,29 +60,66 @@ class UpdateBnBView(TemplateView):
         capacity = request.POST.get('capacity', 0)
         category_id = request.POST.getlist('category-id', [])
         service_id = request.POST.getlist('service-id', [])
-        service_id = request.POST.getlist('rule-id', [])
+        rule_id = request.POST.getlist('rule-id', [])
         description = request.POST.get('description', '')
         bnbid = request.POST.get('bnbid', -1)
-        print(add_img)
+        print("d,s", add_img)
 
         try:
             # Cập nhật thông tin BnB
             bnb = get_bnb(bnbid)
             if bnb is None:
                 return JsonResponse({'success': False})
+
             bnb.name = name
             bnb.description = description
-            bnb.price = price
-            bnb.capacity = capacity
+            if float(price) >= 0:
+                bnb.price = price
+            if int(capacity) >= 0:
+                bnb.capacity = capacity
+
+            #xóa hình ảnh
             for i in delete_img:
                 # Lấy hình ảnh cụ thể và xóa nó
                 image_to_delete = bnb.image_set.get(id=i)  # Lấy hình ảnh có ID tương ứng
-                image_to_delete.delete()  # Xóa hình ảnh
-            for img in add_img:
-                print(img.url)
+                image_to_delete.delete()
+
+            #thêm hình ảnh
+            for file in add_img:
+
+                # Tải tệp lên Cloudinary và nhận URL
+                cloudinary_response = upload(file)
+                file_url = cloudinary_response['secure_url']  # Lấy URL an toàn
+
+                print(file_url)
                 # Tạo đối tượng Image mới cho mỗi tệp hình ảnh
-                image_to_add = Image(url=img.url, product=bnb)
+                image_to_add = Image(url=file_url, product=bnb)
                 image_to_add.save()  # Lưu đối tượng Image vào cơ sở dữ liệu
+
+            #xóa category cũ trong bnb
+            bnb.category.clear()
+            #thêm category vào lại
+            for cid in category_id:
+                category = Category.objects.get(id=cid)  # Lấy category theo id
+                if not bnb.category.filter(id=cid).exists():  # Kiểm tra nếu chưa tồn tại
+                    bnb.category.add(category)  # Thêm category vào bnb
+
+            #xóa service cũ trong bnb
+            bnb.service.clear()
+            #thêm service vào lại
+            for sid in service_id:
+                service = Service.objects.get(id=sid)
+                if not bnb.service.filter(id=sid).exists():
+                    bnb.service.add(service)
+
+            # xóa service cũ trong bnb
+            bnb.rule.clear()
+            # thêm service vào lại
+            for rid in rule_id:
+                rule = Rule.objects.get(id=rid)
+                if not bnb.rule.filter(id=rid).exists():
+                    bnb.rule.add(rule)
+
             bnb.save()
             return JsonResponse({'success': True})
         except Exception as e:
