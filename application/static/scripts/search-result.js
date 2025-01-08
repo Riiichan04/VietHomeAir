7a305ce3a909347ea974b2f62724ddf88bbdb8d0
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2025-01-08 14:31:34 +0700

    Hoàn thành chức năng tìm kiếm theo tên bnb và tích hợp bản đồ.

--- application/static/scripts/search-result.js
+++ application/static/scripts/search-result.js
@@ -1,8 +1,10 @@
 $("#filter").click(function () {
+    $("#overlay").css("visibility","visible")
     $("#filter-container").removeClass('d-none');
 })
 
 $(".btn-close").click(function (){
+    $("#overlay").css("visibility","hidden")
     $('#filter-container').addClass('d-none');
 })
 $(".select-option").click(function (){
