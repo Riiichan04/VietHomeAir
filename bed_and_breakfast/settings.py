eb8f2763c57fec3bd9a39a2a0dd4a2196e15696e
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-01-23 12:02:25 +0700

    Cập nhật SECRET_KEY

--- bed_and_breakfast/settings.py
+++ bed_and_breakfast/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%qnh4q!zryy5j3=ecii_m!f$pfx(3orn6ejyrnb#*2w59d@wl'
+# SECRET_KEY = 'django-insecure-u%qnh4q!zryy5j3=ecii_m!f$pfx(3orn6ejyrnb#*2w59d@wl' #Không còn dùng nữa
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
